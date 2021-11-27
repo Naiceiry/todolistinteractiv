23006657cbdb8d1b3eb9423fe6ea8cfb4a5076c1
Naiceiry <naiceirymp@gmail.com>
2021-11-27 14:00:23 +0000

    no corre

--- src/app.py
+++ src/app.py
@@ -1,27 +1,24 @@
 from flask import Flask, request, jsonify , json
 app = Flask(__name__)
 
-
 todos = [
   { "label": "My first task", "done": False }
 ]
 
-
-
 @app.route('/todos', methods=['GET'])
-def hello_world():
-  json_text = jsonify(todos)
-  return json_text
+  def hello_world():
+    json_text = jsonify(todos)
+    return json_text
 
 @app.route('/todos', methods=['POST'])
-def add_new_todo():
+  def add_new_todo():
     request_body = request.data
     decoded_object = json.loads(request_body)
     todos.append(decoded_object)
     json_text = jsonify(todos)
     return json_text
 
-# Estas dos líneas siempre seben estar al final de tu archivo app.py.
+# # Estas dos líneas siempre seben estar al final de tu archivo app.py.
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
